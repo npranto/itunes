79d668e3606d3cb6ae975e6db20be769c583f4b0
Nazmuz Shakib Pranto <npranto@gmail.com>
2016-06-02 16:54:48 -0500

    itunes is now complete

--- js/itunesService.js
+++ js/itunesService.js
@@ -8,5 +8,36 @@ angular.module('itunes').service('itunesService', function($http, $q){
   //You can return the http request or you can make your own promise in order to manipulate the data before you resolve it.
 
     //Code here
+    this.getData = function(artistName){
+    	var dfd = $q.defer();
+
+    	$http({
+    		method: 'JSONP',
+    		url: 'https://itunes.apple.com/search?term=' + artistName + '&callback=JSON_CALLBACK'
+    	})
+
+    	.then(function(response){
+    		console.log(response);
+
+    		var data = response.data.results;
+    		var filterSongs = [];
+
+
+    		for (var i = 0; i < data.length; i++) {
+    				var songObj = {
+    					Artist: data[i].artistName,
+    					Collection: data[i].collectionName,
+    					AlbumArt: data[i].artworkUrl30,
+    					Type: data[i].kind,
+    					CollectionPrice: data[i].collectionPrice,
+    					Play: data[i].previewUrl
+    			}
+    			filterSongs.push(songObj);
+    		}
+    		dfd.resolve(filterSongs);
+    	});
+    	return dfd.promise;
+    	
+    }
     
 });
--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -15,7 +15,16 @@ angular.module('itunes').controller('mainCtrl', function($scope, itunesService){
         {field: 'CollectionPrice', displayName: 'Collection Price'},
       ]
   };
+   
+   
 
+    $scope.getSongData = function(){
+      itunesService.getData($scope.artist)
+      .then(function(data){
+          $scope.songData = data;
+          console.log($scope.songData);
+        })
+    }
   //Our controller is what's going to connect our 'heavy lifting' itunesService with our view (index.html) so our user can see the results they get back from itunes.
 
   //First inject itunesService into your controller.
@@ -23,12 +32,12 @@ angular.module('itunes').controller('mainCtrl', function($scope, itunesService){
     //code here
 
 
+
   //Now write a function that will call the method on the itunesService that is responsible for getting the data from iTunes, whenever the user clicks the submit button
   //*remember, that method should be expecting an artist name. The artist name is coming from the input box on index.html, head over there and check if that input box is tied to any specific model we could use.
   //Also note that that method should be retuning a promise, so you could use .then in this function.
     
     //Code here
-
 
   //Check that the above method is working by entering a name into the input field on your web app, and then console.log the result
 
